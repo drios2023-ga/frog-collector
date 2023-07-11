43f44606d4d104fdf7fc1bdc6adeb37ae6d54075
drios2023-ga <drios111-general@yahoo.com>
2023-07-11 15:04:01 -0500

    added comments for presentation

--- main_app/models.py
+++ main_app/models.py
@@ -9,8 +9,13 @@ class Frog(models.Model):
     description = models.TextField(max_length=250)
     age=models.IntegerField()
 
+    # returns a readable representation of
+    # an instance of an object
     def __str__(self):
         return f'{self.name} ({self.id})'
 
+    # calculates the url automatically
+    # pass in self, an instance of the class for this model
+    # frog_id is the variable in routes/controller
     def get_absolute_url(self):
         return reverse('detail', kwargs={'frog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -14,33 +14,30 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-# def frogs_index(request):
-#   # We pass data to a template very much like we did in Express!
-#   return render(request, 'frogs/index.html', {
-#     'frogs': frogs
-#   })
-
 def frogs_index(request):
     frogs = Frog.objects.all() # Retrieve all frogs
     return render(request, 'frogs/index.html', 
     { 
-        'frogs': frogs 
+        'frogs': frogs  #frogs on the page = the value of this key
     }
 )
 
 def frogs_detail(request, frog_id):
   frog = Frog.objects.get(id=frog_id)
-  return render(request, 'frogs/detail.html', { 'frog': frog })
+  return render(request, 'frogs/detail.html', { 'frog': frog }) #frog is the variable we pass to page
+
+
+# we import createview, updateview and delete view at the top
+# imports out-of-the-box CRUD functionality
 
 class FrogCreate(CreateView):
   model = Frog
-  fields = '__all__'
-  # success_url = '/frogs/{frog_id}'
+  fields = '__all__' #all fields of model frog
 
 class FrogUpdate(UpdateView):
   model = Frog
-  fields = ['breed', 'description', 'age']
+  fields = ['breed', 'description', 'age'] #fields offered on the page from model
 
 class FrogDelete(DeleteView):
   model = Frog
-  success_url = '/frogs'
+  success_url = '/frogs' 
